1e672329c2fe7bb8c7504e4551073c8f704a996f
QuangHa797979 <qngha7@gmail.com>
2022-08-25 15:50:07 +0700

    change

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=quang
+export POSTGRES_PASSWORD=quanghaha
+export POSTGRES_HOST=cdr.c5rl4ztuugdg.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-16082022-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=qngha7
+export JWT_SECRET=hello
+export URL=http://localhost:8100
---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,16 @@
+export const config = {
+  'username': 'quang',
+  'password': 'quanghaha',
+  'database': 'postgres',
+  'host': 'cdr.c5rl4ztuugdg.us-west-2.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-west-2',
+  'aws_profile': 'default',
+  'access_key_id': 'AKIASVAZ6H7QZYMQHW33',
+  'secret_access_key': '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',
+  'aws_media_bucket': 'test-16082022-dev',
+  'url': 'http://localhost:8080/',
+  'jwt': {
+    'secret': 'hello',
+  }
+}
